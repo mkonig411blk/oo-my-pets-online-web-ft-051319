71cfc271ae38c87c0a1c50a5e570118f754e0cb1
Margot Konig <margot.konig@blackrock.com>
2019-05-30 16:28:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -71,7 +71,7 @@ class Owner
     fish_count = @pets[:fishes].count
     dog_count = @pets[:dogs].count
     cat_count = @pets[:cats].count
-    puts "I have #{fish_count} fish, #{dog_count} dog(s), and #{cat_count} cat(s)."
+     "I have #{fish_count} fish, #{dog_count} dog(s), and #{cat_count} cat(s)."
   end
   
   
