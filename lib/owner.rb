016a179b2c864ed8f27aa1a06ab28777cd263d38
Margot Konig <margot.konig@blackrock.com>
2019-05-30 16:12:41 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,27 @@
 class Owner
-  # code goes here
+  attr_reader :species
+  attr_accessor :name, :pets
+  @@all = []
+  
+  def initialize(species)
+    @species = species
+     @@all << self
+     @pets = {fishes: [], cats: [], dogs: []}
+  end
+  
+  def say_species
+     "I am a #{@species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
 end
